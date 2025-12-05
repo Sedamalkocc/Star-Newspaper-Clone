2117dc4c5dce2505e38d61baa01f45be8ca1082b
Sedamalkocc <sedaamalkocc@gmail.com>
2025-12-05 13:14:51 +0300

    star clone project

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+
+  const $slider = $('.slick-breaking');
+
+  if ($slider.length === 0) {
+    console.warn('⚠️ .slick-breaking bulunamadı!');
+    return;
+  }
+
+  $slider.slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  arrows: true,
+  centerMode: false,
+  adaptiveHeight: false,
+  infinite: true,
+    speed: 700,
+    cssEase: 'linear',
+
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    draggable: true,
+    swipe: true,
+
+    responsive: [
+      { breakpoint: 992, settings: { slidesToShow: 2, slidesToScroll: 2 } },
+      { breakpoint: 576, settings: { slidesToShow: 1, slidesToScroll: 1 } }
+    ]
+  });
+
+  $slider.slick('slickPlay');
+
+});
+
+
+$('.headline-thumb').on('mouseenter', function () {
+
+  const img = $(this).attr('data-img');
+  const title = $(this).attr('data-title');
+
+  $('#mainHeadlineImage').attr('src', img);
+  $('#mainHeadlineTitle').text(title);
+
+  $('.headline-thumb').removeClass('active');
+  $(this).addClass('active');
+
+});
